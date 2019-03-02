991c5baee347d934a072d3ea0746a04eb6c34bc2
dehui333 <dehui333@gmail.com>
2019-03-02 15:59:57 +0800

    added the list of projects to addressbook

--- src/main/java/seedu/address/model/employee/exceptions/DuplicateEmployeeException.java
+++ src/main/java/seedu/address/model/employee/exceptions/DuplicateEmployeeException.java
@@ -2,7 +2,7 @@ package seedu.address.model.employee.exceptions;
 
 /**
  * Signals that the operation will result in duplicate employees(projects are considered duplicates if they have the
- * same name).
+ * same identity).
  */
 public class DuplicateEmployeeException extends RuntimeException {
     public DuplicateEmployeeException() {
