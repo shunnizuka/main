35b4ebf20bbd616c083c6f6d6504b0e12b0d7ce3
dehui333 <dehui333@gmail.com>
2019-03-02 22:12:17 +0800

    changed DeleteCommand.MESSAGE_USAGE

--- src/main/java/seedu/address/logic/commands/DeleteCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteCommand.java
@@ -10,10 +10,6 @@ public abstract class DeleteCommand extends Command {
     public static final String MESSAGE_USAGE = COMMAND_WORD + " employee/project"
             + ": Deletes the employee/project identified by the index number/project name used in"
             + " the displayed employee/project list.\n"
-            + "For delete employee:\n"
-            + "Parameters: INDEX (must be a positive integer)\n"
-            + "Example: " + COMMAND_WORD + " employee" + " 1\n"
-            + "For delete project:\n"
-            + "Parameters: PROJECT_NAME\n"
-            + "Example: " + COMMAND_WORD + " project" + " Apollo";
+            + DeleteEmployeeCommand.MESSAGE_USAGE + "\n"
+            + DeleteProjectCommand.MESSAGE_USAGE;
 }
