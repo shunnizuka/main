d9ec8209b6c760fae4c3f205c5026de4ffaeadc4
jeffgan96 <jeffganmr@Hotmail.com>
2019-03-02 17:07:09 +0800

    add listcommandparser to parse list commands

---
+++ src/main/java/seedu/address/logic/parser/ListCommandParser.java
@@ -0,0 +1,54 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import seedu.address.logic.commands.ListCommand;
+import seedu.address.logic.commands.ListEmployeeCommand;
+import seedu.address.logic.commands.ListProjectCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.employee.NameContainsKeywordsPredicate;
+
+public class ListCommandParser implements Parser<ListCommand> {
+
+        private static final Pattern LIST_COMMAND_FORMAT = Pattern.compile("(?<type>\\S+)");
+        /**
+         * Parses the given {@code String} of arguments in the context of the ListCommand
+         * and returns a ListCommand object for execution.
+         * @throws ParseException if the user input does not conform the expected format
+         */
+
+        public ListCommand parse(String args) throws ParseException {
+            String trimmedArgs = args.trim();
+            final Matcher matcher = LIST_COMMAND_FORMAT.matcher(trimmedArgs);
+
+            if (!matcher.matches()) {
+                throw new ParseException(
+                        String.format(MESSAGE_INVALID_COMMAND_FORMAT, ListCommand.MESSAGE_USAGE));
+            }
+
+            final String type = matcher.group("type");
+
+            if (type.isEmpty()) {
+                throw new ParseException(
+                        String.format(MESSAGE_INVALID_COMMAND_FORMAT, ListCommand.MESSAGE_USAGE));
+            }
+
+            switch (type) {
+
+                case ListEmployeeCommand.LIST_EMPLOYEE_KEYWORD:
+                    return new EmployeeCommand(new NameContainsKeywordsPredicate(Arrays.asList(nameKeywords)));
+
+                case ListProjectCommand.LIST_PROJECT_KEYWORD:
+                    return new FindEmployeeCommand(new NameContainsKeywordsPredicate(Arrays.asList(nameKeywords)));
+
+
+                default:
+                    return new FindEmployeeCommand(new NameContainsKeywordsPredicate(Arrays.asList(nameKeywords)));
+            }
+        }
+
+    }
