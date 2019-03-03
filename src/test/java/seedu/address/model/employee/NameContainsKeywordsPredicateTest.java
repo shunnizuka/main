95db2c5768f35e36ee755cdeb81c242d17ded742
x240\Shune <smiley.slyt@gmail.com>
2019-03-03 17:13:29 +0800

    Fix formatting

--- src/test/java/seedu/address/model/employee/NameContainsKeywordsPredicateTest.java
+++ src/test/java/seedu/address/model/employee/NameContainsKeywordsPredicateTest.java
@@ -73,7 +73,8 @@ public class NameContainsKeywordsPredicateTest {
         assertFalse(predicate.test(new EmployeeBuilder().withName("Alice Bob").build()));
 
         // Keywords match phone, email and address, but does not match name
-        predicate = new EmployeeNameContainsKeywordsPredicate(Arrays.asList("12345", "alice@email.com", "Main", "Street"));
+        predicate = new EmployeeNameContainsKeywordsPredicate(Arrays.asList("12345", "alice@email.com",
+            "Main", "Street"));
         assertFalse(predicate.test(new EmployeeBuilder().withName("Alice").withPhone("12345")
             .withEmail("alice@email.com").withAddress("Main Street").build()));
     }
