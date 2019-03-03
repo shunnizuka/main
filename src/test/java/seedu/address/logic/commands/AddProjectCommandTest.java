aee5710774dbab92df7062b4f9dac5ef69c68d4e
Jothi Pillay <jothipillay1996@gmail.com>
2019-03-03 23:26:27 +0800

    correct formatting errors

--- src/test/java/seedu/address/logic/commands/AddProjectCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddProjectCommandTest.java
@@ -1,5 +1,21 @@
 package seedu.address.logic.commands;
 
+import static java.util.Objects.requireNonNull;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static seedu.address.testutil.TypicalProjects.PROJECT_ALICE;
+import static seedu.address.testutil.TypicalProjects.PROJECT_BENSON;
+
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.function.Predicate;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
 import javafx.beans.property.ReadOnlyProperty;
 import javafx.collections.ObservableList;
 import seedu.address.commons.core.GuiSettings;
@@ -12,22 +28,6 @@ import seedu.address.model.ReadOnlyUserPrefs;
 import seedu.address.model.employee.Employee;
 import seedu.address.model.project.Project;
 import seedu.address.testutil.ProjectBuilder;
-
-import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.function.Predicate;
-
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.rules.ExpectedException;
-
-import static java.util.Objects.requireNonNull;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-import static seedu.address.testutil.TypicalProjects.PROJECT_ALICE;
-import static seedu.address.testutil.TypicalProjects.PROJECT_BENSON;
 
 public class AddProjectCommandTest {
 
