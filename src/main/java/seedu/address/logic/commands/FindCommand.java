f64511e363a0c3248bc274564e531365651b3af3
x240\Shune <smiley.slyt@gmail.com>
2019-03-03 16:44:03 +0800

    Update javadoc

--- src/main/java/seedu/address/logic/commands/FindCommand.java
+++ src/main/java/seedu/address/logic/commands/FindCommand.java
@@ -1,5 +1,9 @@
 package seedu.address.logic.commands;
 
+/**
+ * Represent a find command with hidden internal logic and the ability to be executed.
+ */
+
 public abstract class FindCommand extends Command{
 
     public static final String COMMAND_WORD = "find";
--- src/main/java/seedu/address/logic/commands/FindEmployeeCommand.java
+++ src/main/java/seedu/address/logic/commands/FindEmployeeCommand.java
@@ -8,7 +8,7 @@ import seedu.address.model.Model;
 import seedu.address.model.employee.EmployeeNameContainsKeywordsPredicate;
 
 /**
- * Finds and lists all persons in address book whose name contains any of the argument keywords.
+ * Finds and lists all employees in PocketProject whose name contains any of the argument keywords.
  * Keyword matching is case insensitive.
  */
 public class FindEmployeeCommand extends FindCommand {
