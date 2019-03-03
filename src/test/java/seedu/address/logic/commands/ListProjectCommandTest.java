ddd72ae71409ef5d3ee868a91350221c6f34a7f1
jeffgan96 <jeffganmr@Hotmail.com>
2019-03-03 23:33:19 +0800

    added test for list project command

---
+++ src/test/java/seedu/address/logic/commands/ListProjectCommandTest.java
@@ -0,0 +1,44 @@
+package seedu.address.logic.commands;
+
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.testutil.TypicalEmployees.getTypicalAddressBookWithEmployees;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import seedu.address.logic.CommandHistory;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+
+/**
+ * Contains integration tests (interaction with the Model) and unit tests for ListEmployeeCommand.
+ */
+public class ListProjectCommandTest {
+
+    private Model model;
+    private Model expectedModel;
+    private CommandHistory commandHistory = new CommandHistory();
+
+    @Before
+    public void setUp() {
+        model = new ModelManager(getTypicalAddressBookWithEmployees(), new UserPrefs());
+        expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
+    }
+
+    @Test
+    public void execute_listIsNotFiltered_showsSameList() {
+        assertCommandSuccess(new ListProjectCommand(), model, commandHistory,
+                ListProjectCommand.MESSAGE_SUCCESS, expectedModel);
+    }
+
+    //TODO: run test after listener for projects has been added to the UI
+    /*
+    @Test
+    public void execute_listIsFiltered_showsEverything() {
+        showEmployeeAtIndex(model, INDEX_FIRST_EMPLOYEE);
+        assertCommandSuccess(new ListProjectCommand(), model, commandHistory, ListProjectCommand.MESSAGE_SUCCESS,
+                expectedModel);
+    }
+    */
+}
