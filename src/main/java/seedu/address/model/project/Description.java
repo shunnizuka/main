c03efe9d47ef50da0509ee31a34db8ecdb9b0bc5
x240\Shune <smiley.slyt@gmail.com>
2019-03-10 15:58:41 +0800

    Add description class for project

---
+++ src/main/java/seedu/address/model/project/Description.java
@@ -0,0 +1,66 @@
+package seedu.address.model.project;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
+
+/**
+ * Description of the project. A summary of what the project is about
+ */
+
+public class Description {
+
+    public static final String MESSAGE_CONSTRAINTS =
+        "Description should only contain alphanumeric characters and spaces, and it should not be blank";
+
+    /*
+     * The first character of the description must not be a whitespace,
+     * otherwise " " (a blank string) becomes a valid input.
+     */
+    public static final String VALIDATION_REGEX = "[\\p{Alnum}][\\p{Alnum} ]*";
+
+    public final String description;
+
+    /**
+     * Empty Constructor for when description is not added to the project yet
+     */
+    public Description() {
+        this.description = "";
+    }
+
+    /**
+     * Constructor for when adding description to the project
+     */
+    public Description(String desc) {
+        requireNonNull(desc);
+        checkArgument(isValidDescription(desc), MESSAGE_CONSTRAINTS);
+        this.description = desc;
+    }
+
+    /**
+     * Returns true if a given string is a valid name.
+     */
+    public static boolean isValidDescription(String input) {
+        return input.matches(VALIDATION_REGEX);
+    }
+
+    @Override
+    public String toString() {
+        return description;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+            || (other instanceof Description // instanceof handles nulls
+            && description.equals(((Description) other).description)); // state check
+    }
+
+    @Override
+    public int hashCode() {
+        return description.hashCode();
+    }
+
+
+
+
+}
--- src/main/java/seedu/address/model/project/Project.java
+++ src/main/java/seedu/address/model/project/Project.java
@@ -13,6 +13,7 @@ public class Project {
     private final List<Milestone> milestone;
     private final Client client;
     private final Deadline deadline;
+    private final Description description;
 
     /**
      * Constructor for each Project Object.
@@ -22,6 +23,7 @@ public class Project {
         this.client = c;
         this.deadline = d;
         milestone = new ArrayList<>();
+        this.description = new Description();
     }
 
     /**
@@ -32,6 +34,19 @@ public class Project {
         this.client = c;
         this.deadline = d;
         this.milestone = m;
+        this.description = new Description();
+    }
+
+    /**
+     * Constructor specifying description too.
+     * TODO: edit accordingly for addto function, I'm not sure how you all want to implement
+     */
+    public Project (ProjectName pn, Client c, Deadline d, List<Milestone> m, Description desc) {
+        this.projectName = pn;
+        this.client = c;
+        this.deadline = d;
+        this.milestone = m;
+        this.description = desc;
     }
 
     public ProjectName getProjectName() {
@@ -46,6 +61,8 @@ public class Project {
     public Deadline getDeadline() {
         return deadline;
     }
+    public Description getDescription() {
+        return description; }
 
     /**
      * Returns true if both projects have the same name.
@@ -56,7 +73,7 @@ public class Project {
         }
 
         return otherProject != null
-                && otherProject.getProjectName().equals(getProjectName());
+            && otherProject.getProjectName().equals(getProjectName());
     }
 
     /**
@@ -78,15 +95,16 @@ public class Project {
 
         Project otherProject = (Project) other;
         return otherProject.getProjectName().equals(getProjectName())
-                && otherProject.getClient().equals(getClient())
-                && otherProject.getDeadline().equals(getDeadline());
+            && otherProject.getClient().equals(getClient())
+            && otherProject.getDeadline().equals(getDeadline())
+            && otherProject.getDescription().equals(getDescription());
 
     }
 
     @Override
     public int hashCode() {
         // use this method for custom fields hashing instead of implementing your own
-        return Objects.hash(projectName, client, deadline);
+        return Objects.hash(projectName, client, deadline, description);
     }
 
     @Override
@@ -94,10 +112,11 @@ public class Project {
 
         final StringBuilder builder = new StringBuilder();
         builder.append(getProjectName())
-                .append(" Client: ")
-                .append(getClient())
-                .append(" Deadline: ")
-                .append(getDeadline());
+            .append(" Client: ")
+            .append(getClient())
+            .append(" Deadline: ")
+            .append(getDeadline())
+            .append(getDescription());
 
         return builder.toString();
     }
