1338d9dab0e501b2ed594dd916cacb477c8b06f8
dehui333 <dehui333@gmail.com>
2019-03-13 13:57:33 +0800

    RemoveFromCommandParser.java: attempted to implement parser

--- src/main/java/seedu/address/logic/parser/RemoveFromCommandParser.java
+++ src/main/java/seedu/address/logic/parser/RemoveFromCommandParser.java
@@ -1,7 +1,61 @@
 package seedu.address.logic.parser;
 
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.HelpCommand;
+import seedu.address.logic.commands.RemoveEmployeeFromCommand;
+import seedu.address.logic.commands.RemoveFromCommand;
+import seedu.address.logic.commands.RemoveMilestoneFromCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.project.ProjectName;
+
 /**
  * Parses input arguments and creates a new RemoveFromCommand object
  */
-public class RemoveFromCommandParser {
+public class RemoveFromCommandParser implements Parser<RemoveFromCommand> {
+
+    /**
+     * Used for separation of type keyword and args.
+     */
+    private static final Pattern REMOVE_FROM_COMMAND_FORMAT = Pattern.compile("(?<project>(\\S+\\s)+)"
+        + "(?<keyword>employee\\s|milestone\\s)(?<arguments>.*)");
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the RemoveFromCommand
+     * and returns an RemoveFromCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public RemoveFromCommand parse(String args) throws ParseException {
+
+        final Matcher matcher = REMOVE_FROM_COMMAND_FORMAT.matcher(args.trim());
+        if (!matcher.matches()) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, HelpCommand.MESSAGE_USAGE));
+        }
+        final ProjectName projectName = new ProjectName(matcher.group("project").trim());
+        final String keyword = matcher.group("keyword");
+        final String arguments = matcher.group("arguments");
+
+        if (keyword.equals(RemoveEmployeeFromCommand.REMOVE_EMPLOYEE_KEYWORD)) {
+            try {
+                Index index = ParserUtil.parseIndex(arguments.trim());
+                return new RemoveEmployeeFromCommand(index, projectName);
+            } catch (ParseException pe) {
+                throw new ParseException(
+                        String.format(MESSAGE_INVALID_COMMAND_FORMAT, RemoveEmployeeFromCommand.MESSAGE_USAGE), pe);
+            }
+        } else if (keyword.equals(RemoveMilestoneFromCommand.REMOVE_MILESTONE_KEYWORD)) {
+            Index index = ParserUtil.parseIndex(arguments.trim());
+            return new RemoveMilestoneFromCommand(index, projectName);
+
+        } else {
+            throw new ParseException (
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, RemoveMilestoneFromCommand.MESSAGE_USAGE)
+            );
+        }
+    }
+
 }
