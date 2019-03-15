0c2f1bd950e90aa1b20f817857f9516f7cd29861
jeffgan96 <jeffganmr@Hotmail.com>
2019-03-15 22:32:56 +0800

    updated button functionality and changed info panel to stackpane

--- src/main/java/seedu/address/ui/DetailsPanel.java
+++ src/main/java/seedu/address/ui/DetailsPanel.java
@@ -1,20 +1,30 @@
 package seedu.address.ui;
 
+import java.util.List;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.Region;
+import javafx.scene.layout.StackPane;
 
+/**
+ * Panel of the main window to show all information or employees and projects
+ */
 public class DetailsPanel extends UiPart<Region> {
 
     public static final String FXML = "DetailsPanel.fxml";
+    private List<Pane> contentPanels;
 
     @FXML
     private BorderPane borderPanel;
 
     @FXML
-    private ScrollPane informationPanel;
+    private StackPane informationPanel;
+
     @FXML
     private Button prevBtn;
 
@@ -23,6 +33,27 @@ public class DetailsPanel extends UiPart<Region> {
 
     public DetailsPanel() {
         super(FXML);
+        initPrevBtn();
+        initNextBtn();
+    }
 
+
+
+    private void initPrevBtn() {
+        prevBtn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+
+            }
+        });
+    }
+
+    private void initNextBtn() {
+        nextBtn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+
+            }
+        });
     }
 }
