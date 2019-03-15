0e17027549ac2dbc305acf28892c13efeade5326
jeffgan96 <jeffganmr@Hotmail.com>
2019-03-16 00:58:42 +0800

    added methods for detailspanel

--- src/main/java/seedu/address/ui/DetailsPanel.java
+++ src/main/java/seedu/address/ui/DetailsPanel.java
@@ -1,10 +1,12 @@
 package seedu.address.ui;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
@@ -17,7 +19,10 @@ import javafx.scene.layout.StackPane;
 public class DetailsPanel extends UiPart<Region> {
 
     public static final String FXML = "DetailsPanel.fxml";
-    private List<Pane> contentPanels;
+    private static final int INITIAL_PANEL_INDEX = 0;
+
+    private List<Node> contentList;
+    private int currentPanelIndex;
 
     @FXML
     private BorderPane borderPanel;
@@ -35,15 +40,39 @@ public class DetailsPanel extends UiPart<Region> {
         super(FXML);
         initPrevBtn();
         initNextBtn();
+        initDefaultView();
+        this.currentPanelIndex = INITIAL_PANEL_INDEX;
     }
 
+    /**
+     * Initialises a default list of panels to show when nothing is selected.
+     *
+     */
+    private void initDefaultView() {
+        Pane pane = new Pane();
+        contentList = new ArrayList<>();
+        contentList.add(pane);
+        updateInformationPanel(pane);
+    }
 
+    private void updateInformationPanel(Node e) {
+        informationPanel.getChildren().add(e);
+    }
+
+    /**
+     * Updates the content in the information panel to the list of content provided.
+     * @param content
+     */
+    private void setContent(List<Node> content) {
+        contentList.clear();
+        contentList.addAll(content);
+    }
 
     private void initPrevBtn() {
         prevBtn.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
-
+                showPrevPanel();
             }
         });
     }
@@ -52,8 +81,20 @@ public class DetailsPanel extends UiPart<Region> {
         nextBtn.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
-
+                showNextPanel();
             }
         });
+    }
+
+    private void showPrevPanel() {
+        int nextIndex = currentPanelIndex - 1;
+        currentPanelIndex = nextIndex % contentList.size();
+        updateInformationPanel(contentList.get(currentPanelIndex));
+    }
+
+    private void showNextPanel() {
+        int nextIndex = currentPanelIndex + 1;
+        currentPanelIndex = nextIndex % contentList.size();
+        updateInformationPanel(contentList.get(currentPanelIndex));
     }
 }
