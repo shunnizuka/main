c55f5f8a44a161319ab5f1ca01d3af40adb366c0
ditan96 <ditby1006@yahoo.com>
2019-03-17 20:03:26 +0800

    Fix formatting

--- src/main/java/seedu/address/logic/commands/FindCommand.java
+++ src/main/java/seedu/address/logic/commands/FindCommand.java
@@ -8,7 +8,7 @@ public abstract class FindCommand extends Command {
 
     public static final String COMMAND_WORD = "find";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + "employee/project/skill"
+    public static final String MESSAGE_USAGE = COMMAND_WORD + " employee/project/skill"
         + ": Finds all employees/project/skill which contain "
         + "the specified arguments (case-insensitive) and displays them as a list with index numbers.\n"
         + "Parameters: ARGUMENTS \n"
--- src/main/java/seedu/address/ui/MainWindow.java
+++ src/main/java/seedu/address/ui/MainWindow.java
@@ -128,7 +128,8 @@ public class MainWindow extends UiPart<Stage> {
         resultDisplay = new ResultDisplay();
         resultDisplayPlaceholder.getChildren().add(resultDisplay.getRoot());
 
-        StatusBarFooter statusBarFooter = new StatusBarFooter(logic.getPocketProjectFilePath(), logic.getPocketProject());
+        StatusBarFooter statusBarFooter =
+                new StatusBarFooter(logic.getPocketProjectFilePath(), logic.getPocketProject());
         statusbarPlaceholder.getChildren().add(statusBarFooter.getRoot());
 
         CommandBox commandBox = new CommandBox(this::executeCommand, logic.getHistory());
