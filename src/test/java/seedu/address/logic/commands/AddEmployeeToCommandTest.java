26147485070bce5df6f773a8d8c6d2db63fa2c8a
Jothi Pillay <jothipillay1996@gmail.com>
2019-03-17 21:27:04 +0800

    update switch cases in address book parser

--- src/test/java/seedu/address/logic/commands/AddEmployeeToCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddEmployeeToCommandTest.java
@@ -6,14 +6,28 @@ import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
 import seedu.address.testutil.TestUtil;
 
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
 /**
  * Contains integration tests (interaction with the Model, UndoCommand and RedoCommand) and unit tests for
  * {@code AddEmployeeToCommand}.
  */
 public class AddEmployeeToCommandTest {
 
-    private Model model = new ModelManager(TestUtil.typicalAddressBook(), new UserPrefs());
+    private static final CommandHistory EMPTY_COMMAND_HISTORY = new CommandHistory();
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
     private CommandHistory commandHistory = new CommandHistory();
+
+    @Test
+    public void constructor_null_throwsNullPointerException() {
+        thrown.expect(NullPointerException.class);
+        new AddEmployeeToCommand(null, null);
+    }
 
 
 }
