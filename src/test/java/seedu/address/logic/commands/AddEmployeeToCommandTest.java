cd1c93ed078e2337613acc246392ceadfac31eec
Jothi Pillay <jothipillay1996@gmail.com>
2019-03-18 11:01:13 +0800

    remove unused import statements

--- src/test/java/seedu/address/logic/commands/AddEmployeeToCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddEmployeeToCommandTest.java
@@ -4,7 +4,6 @@ import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.CommandHistory;
 
-import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 
@@ -18,7 +17,6 @@ import seedu.address.testutil.TypicalProjects;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
