c05c4887925384f1097c20bf148a0def978bee3d
jeffgan96 <jeffganmr@Hotmail.com>
2019-03-22 05:58:12 +0800

    formatting

--- src/main/java/seedu/address/logic/parser/PocketProjectParser.java
+++ src/main/java/seedu/address/logic/parser/PocketProjectParser.java
@@ -20,8 +20,8 @@ import seedu.address.logic.commands.HistoryCommand;
 import seedu.address.logic.commands.ListCommand;
 import seedu.address.logic.commands.RedoCommand;
 import seedu.address.logic.commands.RemoveFromCommand;
-import seedu.address.logic.commands.ViewCommand;
 import seedu.address.logic.commands.UndoCommand;
+import seedu.address.logic.commands.ViewCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
 /**
