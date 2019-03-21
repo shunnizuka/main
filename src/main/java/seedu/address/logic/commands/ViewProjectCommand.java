7d3abf45e05727830d4765114580932bb1daf637
jeffgan96 <jeffganmr@Hotmail.com>
2019-03-22 02:29:59 +0800

    added view project command

---
+++ src/main/java/seedu/address/logic/commands/ViewProjectCommand.java
@@ -0,0 +1,59 @@
+package seedu.address.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+
+import java.util.List;
+
+import seedu.address.commons.core.Messages;
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.CommandHistory;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.project.Project;
+
+/**
+ * Selects a project identified using it's displayed index from the projects list.
+ */
+public class ViewProjectCommand extends ViewCommand {
+
+    public static final String VIEW_PROJECT_KEYWORD = "project";
+
+    public static final String MESSAGE_USAGE = ViewCommand.COMMAND_WORD
+            + VIEW_PROJECT_KEYWORD
+            + ": Selects the project identified by the index number used in the displayed project list.\n"
+            + "Parameters: INDEX (must be a positive integer)\n"
+            + "Example: "
+            + ViewCommand.COMMAND_WORD
+            + VIEW_PROJECT_KEYWORD
+            + " 1";
+
+    public static final String MESSAGE_VIEW_PROJECT_SUCCESS = "Viewing project details at index: %1$s";
+
+    private final Index targetIndex;
+
+    public ViewProjectCommand(Index targetIndex) {
+        this.targetIndex = targetIndex;
+    }
+
+    @Override
+    public CommandResult execute(Model model, CommandHistory history) throws CommandException {
+        requireNonNull(model);
+
+        List<Project> filteredProjectList = model.getFilteredProjectList();
+
+        if (targetIndex.getZeroBased() >= filteredProjectList.size()) {
+            throw new CommandException(Messages.MESSAGE_INVALID_PROJECT_DISPLAYED_INDEX);
+        }
+
+        model.setSelectedProject(filteredProjectList.get(targetIndex.getZeroBased()));
+        return new CommandResult(String.format(MESSAGE_VIEW_PROJECT_SUCCESS, targetIndex.getOneBased()));
+
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof ViewProjectCommand // instanceof handles nulls
+                && targetIndex.equals(((ViewProjectCommand) other).targetIndex)); // state check
+    }
+}
