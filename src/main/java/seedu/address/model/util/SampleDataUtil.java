c9655cea40a50652e7fb24d83e8495c698e080a5
Jothi Pillay <jothipillay1996@gmail.com>
2019-03-28 03:32:09 +0800

    completed refactoring in model component

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -20,22 +20,22 @@ public class SampleDataUtil {
     public static Employee[] getSampleEmployees() {
         return new Employee[] {
             new Employee(new Name("Alex Yeoh"), new Phone("87438807"), new Email("alexyeoh@example.com"),
-                new GitHubAccount("Blk 30 Geylang Street 29, #06-40"),
+                new GitHubAccount("yeohyeoh"),
                 getSkillSet("friends")),
             new Employee(new Name("Bernice Yu"), new Phone("99272758"), new Email("berniceyu@example.com"),
-                new GitHubAccount("Blk 30 Lorong 3 Serangoon Gardens, #07-18"),
+                new GitHubAccount("bernthemall"),
                 getSkillSet("colleagues", "friends")),
             new Employee(new Name("Charlotte Oliveiro"), new Phone("93210283"), new Email("charlotte@example.com"),
-                new GitHubAccount("Blk 11 Ang Mo Kio Street 74, #11-04"),
+                new GitHubAccount("thespiderweb"),
                 getSkillSet("neighbours")),
             new Employee(new Name("David Li"), new Phone("91031282"), new Email("lidavid@example.com"),
-                new GitHubAccount("Blk 436 Serangoon Gardens Street 26, #16-43"),
+                new GitHubAccount("liddavid"),
                 getSkillSet("family")),
             new Employee(new Name("Irfan Ibrahim"), new Phone("92492021"), new Email("irfan@example.com"),
-                new GitHubAccount("Blk 47 Tampines Street 20, #17-35"),
+                new GitHubAccount("relaxandcode"),
                 getSkillSet("classmates")),
             new Employee(new Name("Roy Balakrishnan"), new Phone("92624417"), new Email("royb@example.com"),
-                new GitHubAccount("Blk 45 Aljunied Street 85, #11-31"),
+                new GitHubAccount("chindianattacks"),
                 getSkillSet("colleagues"))
         };
     }
