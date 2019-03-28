9838021ef7f492e4e22747266a1b9f77a10733cf
jeffgan96 <43617425+jeffgan96@users.noreply.github.com>
2019-03-28 17:55:36 +0800

    Update AddUserStoryToCommandSystemTest.java

--- src/test/java/systemtests/AddUserStoryToCommandSystemTest.java
+++ src/test/java/systemtests/AddUserStoryToCommandSystemTest.java
@@ -75,14 +75,14 @@ public class AddUserStoryToCommandSystemTest extends PocketProjectSystemTest {
     }
 
     /**
-     * Executes the {@code AddMilestoneToCommand} that adds {@code milestone} to the model and asserts that the,<br>
+     * Executes the {@code AddUserStoryToCommand} that adds {@code UserStory} to the model and asserts that the,<br>
      * 1. Command box displays an empty string.<br>
      * 2. Command box has the default style class.<br>
-     * 3. Result display box displays the success message of executing {@code AddMilestoneToCommand} with the details of
-     * {@code milestone}.<br>
+     * 3. Result display box displays the success message of executing {@code AddUserStoryToCommand} with the details of
+     * {@code UserStory}.<br>
      * 4. {@code Storage} equal to the corresponding components in
-     * the current model added with {@code milestone}.<br>
-     * 5. Browser url and selected card remain unchanged.<br>
+     * the current model added with {@code UserStory}.<br>
+     * 5. details panel and selected card remain unchanged.<br>
      * 6. Status bar's sync status changes.<br>
      * Verifications 1, 3 and 4 are performed by
      * {@code PocketProjectSystemTest#assertApplicationDisplaysExpected(String, String, Model)}.<br>
@@ -113,7 +113,7 @@ public class AddUserStoryToCommandSystemTest extends PocketProjectSystemTest {
      * 1. Result display box displays {@code expectedResultMessage}.<br>
      * 2. {@code Storage} equal to the corresponding components in
      * {@code expectedModel}.<br>
-     * @see AddMilestoneToCommandSystemTest#assertCommandSuccess(String, Project, Milestone)
+     * @see AddUserStoryeToCommandSystemTest#assertCommandSuccess(String, Project, UserStory)
      */
 
     private void assertCommandSuccess(String command, Model expectedModel, String expectedResultMessage) {
@@ -130,7 +130,7 @@ public class AddUserStoryToCommandSystemTest extends PocketProjectSystemTest {
      * 2. Command box has the error style class.<br>
      * 3. Result display box displays {@code expectedResultMessage}.<br>
      * 4. {@code Storage} remain unchanged.<br>
-     * 5. Browser url, selected card and status bar remain unchanged.<br>
+     * 5. Details panel, selected card and status bar remain unchanged.<br>
      * Verifications 1, 3 and 4 are performed by
      * {@code PocketProjectSystemTest#assertApplicationDisplaysExpected(String, String, Model)}.<br>
      * @see PocketProjectSystemTest#assertApplicationDisplaysExpected(String, String, Model)
