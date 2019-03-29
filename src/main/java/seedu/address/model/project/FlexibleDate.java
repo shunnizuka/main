232baad5e7e6097f0e74daef2b8a1b986a15c4e0
Jothi Pillay <jothipillay1996@gmail.com>
2019-03-30 02:24:25 +0800

    added methods to flexible date class

--- src/main/java/seedu/address/model/project/FlexibleDate.java
+++ src/main/java/seedu/address/model/project/FlexibleDate.java
@@ -1,8 +1,64 @@
 package seedu.address.model.project;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 /**
- * The date used in the pocket project.
+ * The date used in the pocket project that allows flexible entries.
  */
 
 public class FlexibleDate {
+
+    private LocalDateTime targetDate;
+
+    private static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+    public FlexibleDate() {
+        this.targetDate = LocalDateTime.now();
+    }
+
+    public String currentDate() {
+        return DATE_FORMAT.format(targetDate);
+    }
+
+    public String dateNumDaysLater(long numDays) {
+        LocalDateTime newDate = targetDate.plusDays(numDays);
+        return DATE_FORMAT.format(newDate);
+    }
+
+    public String dateNumDaysBefore(long numDays) {
+        LocalDateTime newDate = targetDate.minusDays(numDays);
+        return DATE_FORMAT.format(newDate);
+    }
+
+    public String dateNumWeeksLater(long numWeeks) {
+        LocalDateTime newDate = targetDate.plusWeeks(numWeeks);
+        return DATE_FORMAT.format(newDate);
+    }
+
+    public String dateNumWeeksBefore(long numWeeks) {
+        LocalDateTime newDate = targetDate.minusWeeks(numWeeks);
+        return DATE_FORMAT.format(newDate);
+    }
+
+    public String dateNumMonthsLater(long numMonths) {
+        LocalDateTime newDate = targetDate.plusMonths(numMonths);
+        return DATE_FORMAT.format(newDate);
+    }
+
+    public String dateNumMonthsBefore(long numMonths) {
+        LocalDateTime newDate = targetDate.minusMonths(numMonths);
+        return DATE_FORMAT.format(newDate);
+    }
+
+    public String dateNumYearsLater(long numYears) {
+        LocalDateTime newDate = targetDate.plusYears(numYears);
+        return DATE_FORMAT.format(newDate);
+    }
+
+    public String dateNumYearsBefore(long numYears) {
+        LocalDateTime newDate = targetDate.minusYears(numYears);
+        return DATE_FORMAT.format(newDate);
+    }
+
 }
