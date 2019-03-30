ad616eef425fd494fd17b6f641d2da66681f75fa
Jothi Pillay <jothipillay1996@gmail.com>
2019-03-30 17:16:20 +0800

    updated logic for today, tomorrow, yesterday

--- src/main/java/seedu/address/logic/parser/FlexibleDateParser.java
+++ src/main/java/seedu/address/logic/parser/FlexibleDateParser.java
@@ -5,6 +5,7 @@ import java.util.regex.Pattern;
 
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.project.Deadline;
+import seedu.address.model.project.FlexibleDate;
 
 /**
  * Parses the flexible date input.
@@ -20,6 +21,11 @@ public class FlexibleDateParser {
      * To check if input is given in the fixed format.
      */
     private static final String ALPHANUMERIC_VALIDATION_REGEX = "^\\S+[a-zA-Z0-9]*$";
+
+    private static final int NEXT = 1;
+    private static final int LAST = -1;
+    private static final int CURRENT = 0;
+    private static final int DAY_ZERO = 0;
 
     /**
      * Parses user flexible date input into fixed format string for processing.
@@ -42,20 +48,39 @@ public class FlexibleDateParser {
 
             switch(keyword) {
 
+            case CliSyntax.TODAY_KEYWORD:
+                return formatDate(CURRENT);
 
+            case CliSyntax.TOMORROW_KEYWORD:
+                return formatDate(NEXT);
 
+            case CliSyntax.YESTERDAY_KEYWORD:
+                return formatDate(LAST);
 
-
-
+            default:
+                throw new ParseException(Deadline.MESSAGE_CONSTRAINTS); //TODO CHANGE Message
             }
-
         }
-
-
-
     }
 
     private static boolean isFlexibleInput(String flexibleDateInput) {
         return flexibleDateInput.matches(ALPHANUMERIC_VALIDATION_REGEX);
+    }
+
+    /**
+     * Gets and formats target date based on DD/MM/YYYY
+     * @return formatted target date.
+     */
+    private static String formatDate(int days) {
+
+        FlexibleDate date = new FlexibleDate();
+
+        if (days > DAY_ZERO) {
+            return date.dateNumDaysLater(days);
+        } else if (days < DAY_ZERO) {
+            return date.dateNumDaysBefore(days);
+        } else {
+             return date.currentDate();
+        }
     }
 }
