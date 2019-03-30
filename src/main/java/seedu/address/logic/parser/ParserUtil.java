7351a64266f20271ec736570fcefa75698535d87
Jothi Pillay <jothipillay1996@gmail.com>
2019-03-30 15:51:41 +0800

    created flexible date parser class

--- src/main/java/seedu/address/logic/parser/ParserUtil.java
+++ src/main/java/seedu/address/logic/parser/ParserUtil.java
@@ -171,7 +171,7 @@ public class ParserUtil {
     }
 
     /**
-     * Parses a {@code String name} into a {@code Deadline}.
+     * Parses a {@code String deadline} into a {@code Deadline}.
      * Leading and trailing whitespaces will be trimmed.
      *
      * @throws ParseException if the given {@code deadline} is invalid.
@@ -179,10 +179,23 @@ public class ParserUtil {
     public static Deadline parseDeadline(String deadline) throws ParseException {
         requireNonNull(deadline);
         String trimmedDate = deadline.trim();
+        parseFlexibleDate(trimmedDate);
         if (!Deadline.isValidDate(trimmedDate)) {
             throw new ParseException(Deadline.MESSAGE_CONSTRAINTS);
         }
         return new Deadline(trimmedDate);
+    }
+
+    /**
+     * Parses a flexible input {@code flexibleDate} into a {@code String deadline}.
+     * @param flexibleDate the flexible date string input
+     * @return the flexible date converted to the fixed date format in DD/MM/YYYY
+     * @throws ParseException if the given {@code flexibleDate} is invalid.
+     */
+    public static String parseFlexibleDate(String flexibleDate) throws ParseException {
+
+
+        return "";
     }
 
     /**
