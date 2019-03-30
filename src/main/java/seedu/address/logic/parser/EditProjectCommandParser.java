c193f97634790d9be8f6f119a5648cab4f5ee065
shunnizuka <smiley.slyt@gmail.com>
2019-03-30 16:30:19 +0800

    Add relevant classes

---
+++ src/main/java/seedu/address/logic/parser/EditProjectCommandParser.java
@@ -0,0 +1,8 @@
+package seedu.address.logic.parser;
+
+import seedu.address.logic.commands.EditProjectCommand;
+
+public class EditProjectCommandParser implements Parser<EditProjectCommand> {
+    
+}
+
