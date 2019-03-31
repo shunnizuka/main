84bf64db34b4f2c3b484e703debbcfff4abc9c6c
dehui333 <dehui333@gmail.com>
2019-03-31 15:35:30 +0800

    Removed unused imports

--- src/main/java/seedu/address/logic/commands/StatsCommand.java
+++ src/main/java/seedu/address/logic/commands/StatsCommand.java
@@ -1,17 +1,8 @@
 package seedu.address.logic.commands;
 
-import static java.util.Objects.requireNonNull;
-
-import java.util.List;
-
-import seedu.address.commons.core.Messages;
-import seedu.address.commons.core.index.Index;
 import seedu.address.logic.CommandHistory;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
-import seedu.address.model.project.Project;
-import seedu.address.model.project.ProjectName;
-
 /**
  * Displays the number of ongoing and completed projects
  */
