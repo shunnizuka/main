2b20389f7fc710e382857d1a043a2379e639d016
dehui333 <dehui333@gmail.com>
2019-03-31 14:43:17 +0800

    Added CompleteCommandParser.java

---
+++ src/main/java/seedu/address/logic/parser/CompleteCommandParser.java
@@ -0,0 +1,50 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.CompleteCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.project.ProjectName;
+
+/**
+ * Parses input arguments and creates a new CompleteCommand object
+ */
+public class CompleteCommandParser implements Parser<CompleteCommand> {
+
+    /**
+     * Used for separation of delete type word and args.
+     */
+    private static final Pattern COMPLETE_COMMAND_FORMAT = Pattern.compile("(?<arguments>.*)");
+    private static final String INTEGER_STRING_FORMAT = "(-?)[0-9]+";
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the CompleteCommand
+     * and returns a CompleteCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public CompleteCommand parse(String args) throws ParseException {
+
+        final Matcher matcher = COMPLETE_COMMAND_FORMAT.matcher(args.trim());
+        if (!matcher.matches()) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, CompleteCommand.MESSAGE_USAGE));
+        }
+        final String arguments = matcher.group("arguments");
+        String argument = arguments.trim();
+        if (argument.matches(INTEGER_STRING_FORMAT)) {
+            try {
+                Index index = ParserUtil.parseIndex(argument);
+                return new CompleteCommand(index);
+            } catch (ParseException pe) {
+                throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                        CompleteCommand.MESSAGE_USAGE, pe));
+            }
+        } else {
+            return new CompleteCommand(new ProjectName(argument));
+        }
+    }
+
+}
--- src/main/java/seedu/address/logic/parser/PocketProjectParser.java
+++ src/main/java/seedu/address/logic/parser/PocketProjectParser.java
@@ -10,6 +10,7 @@ import seedu.address.logic.commands.AddCommand;
 import seedu.address.logic.commands.AddToCommand;
 import seedu.address.logic.commands.ClearCommand;
 import seedu.address.logic.commands.Command;
+import seedu.address.logic.commands.CompleteCommand;
 import seedu.address.logic.commands.DeleteCommand;
 import seedu.address.logic.commands.EditCommand;
 import seedu.address.logic.commands.ExitCommand;
@@ -62,7 +63,8 @@ public class PocketProjectParser {
 
         case DeleteCommand.COMMAND_WORD:
             return new DeleteCommandParser().parse(arguments);
-
+        case CompleteCommand.COMMAND_WORD:
+            return new CompleteCommandParser().parse(arguments);
         case EditCommand.COMMAND_WORD:
             return new EditCommandParser().parse(arguments);
 
