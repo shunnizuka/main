aff0105a7836fc3c38947ae5ed232079d0861079
ditan96 <31795672+ditan96@users.noreply.github.com>
2019-03-31 23:10:58 +0800

    Fix typo in comment

--- src/main/java/seedu/address/model/project/exceptions/ProjectTaskNotFoundException.java
+++ src/main/java/seedu/address/model/project/exceptions/ProjectTaskNotFoundException.java
@@ -1,6 +1,6 @@
 package seedu.address.model.project.exceptions;
 
 /**
- * Signals that the operation is unable to find the specified employee.
+ * Signals that the operation is unable to find the specified project task.
  */
 public class ProjectTaskNotFoundException extends RuntimeException {}
