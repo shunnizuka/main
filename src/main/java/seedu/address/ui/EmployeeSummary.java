82271e273b24610ade25bf37252957f8b9ee987b
shunnizuka <smiley.slyt@gmail.com>
2019-04-02 01:14:22 +0800

    Style EmployeeSummary panel

--- src/main/java/seedu/address/ui/EmployeeSummary.java
+++ src/main/java/seedu/address/ui/EmployeeSummary.java
@@ -3,7 +3,6 @@ package seedu.address.ui;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.layout.FlowPane;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.Region;
 import seedu.address.model.employee.Employee;
@@ -25,13 +24,21 @@ public class EmployeeSummary extends UiPart<Region> {
     private Label projects;
 
     @FXML
+    private Label email;
+
+    @FXML
+    private Label phone;
+
+    @FXML
     private FlowPane skills;
-    
-    
+
+
 
     public EmployeeSummary(Employee employee) {
         super(FXML);
         name.setText(employee.getName().fullName);
+        phone.setText(employee.getPhone().value);
+        email.setText(employee.getEmail().value);
         employee.getSkills().forEach(skill -> {
             Label label = new Label(skill.skillName);
             label.getStyleClass().add(SideTabPanel.getSkillLabelColor(skill.skillName));
