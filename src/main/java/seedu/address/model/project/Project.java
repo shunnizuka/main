bafdd87a2709bbe71b695f9e6473d1355dda1a84
Jothi Pillay <jothipillay1996@gmail.com>
2019-04-05 17:11:04 +0800

    removed unused imports

--- src/main/java/seedu/address/model/project/Project.java
+++ src/main/java/seedu/address/model/project/Project.java
@@ -1,8 +1,5 @@
 package seedu.address.model.project;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Comparator;
 import java.util.Objects;
 
 import javafx.collections.ObservableList;
