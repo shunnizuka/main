7568e5a3a06f88ba41cef5ba0283ca3dd6cafdc1
shunnizuka <smiley.slyt@gmail.com>
2019-04-15 20:56:34 +0800

    remove comments

--- src/main/java/seedu/address/logic/parser/FindCommandParser.java
+++ src/main/java/seedu/address/logic/parser/FindCommandParser.java
@@ -61,9 +61,6 @@ public class FindCommandParser implements Parser<FindCommand> {
         } else if (keyword.equals(FindAllCommand.FIND_ALL_KEYWORD)) {
             return new FindAllCommand(new ProjectContainsKeywordsPredicate(Arrays.asList(nameKeywords)));
         } else if (keyword.equals(FindDeadlineCommand.FIND_DEADLINE_KEYWORD)) {
-            //to make sure the input is valid and flexible date is converted to dd/mm/yyyy format
-            //TODO how to throw extra argument exception, should only accept one date as input,
-            // currently will show invalid date
             PocketProjectDate inputDeadline = ParserUtil.parseDate(arguments);
             return new FindDeadlineCommand(new ProjectContainsDeadlinePredicate(inputDeadline.date));
         } else {
